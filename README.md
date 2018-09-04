a7806f3289deedfdc8dc2f1a2654671ce3a0e7e0
Matheus Talpe <matheusmtalpe@gmail.com>
2018-09-04 19:37:49 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 * [Instructions](#instructions)
 * [Contributing](#contributing)
+* [ContributorNotes](#contributorNotes)
+* [Important](#important)
+* [How to Play](#howToPlay)
 
 ## Instructions
 
@@ -18,3 +21,31 @@ For specific, detailed instructions, look at the project instructions in the [Ud
 This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
 
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+## ContributorNotes
+
+Hi !
+
+This project has been modified by Matheus Talpe as a final project for the Udacity's Front-End Fundamentals Course.
+
+Below, there's a list of online repositories that are needed to run this game:
+
+* [BootsTrap](https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css)
+* [GoogleFonts](https://fonts.googleapis.com/css?family=Coda)
+
+## Important
+In order to run the game, you'll need an active internet connection. Otherwise the game won't be able to access the above mentioned dependencies and should not run as expected.
+
+## How to Play
+
+* Run the [index.html] file in the game directory.
+
+* Just click on any card to start.
+
+* To win the game, you must match all the pairs.
+
+* If you wish to restart, just press the restart button located at the right side of the "Moves" counter.
+
+* Your score will be based on the amount of moves(clicks on cards) used to match all pairs. The more moves you make, less stars you'll get.  
+
+Good luck !
--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
-/*
- * Create a list that holds all of your cards
- */
+const playAgain = document.querySelector("#playAgain");
+const deck = document.querySelector(".deck");
+const restartButton = document.querySelector(".fa-repeat");
+const starsModal = document.querySelector(".stars-modal");
+const starsMain = document.querySelector(".stars");
+let cards = [...document.getElementsByClassName("card")];
+let clicks = 0;
+let selectedCards = [];
+let matchedPairs = 0;
+let stars = [...document.getElementsByClassName("fa fa-star")];
+let timer = document.querySelector(".timer");
+let seconds = 0;
+let timerId;
+let modal = document.querySelector(".modal");
+let modalTime = document.querySelector(".modal-time");
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -26,13 +31,146 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+/* this functions initializes/restart the game shuffling the cards and initializing
+   some vars. */
+let initializeGame = function() {
+
+  // initializes components from the main screen
+   modal.style.visibility = "collapse";
+   document.querySelector(".moves").textContent = 0;
+
+   timer.innerHTML=("0:0");
+   clearInterval(timerId);
+
+   stars[0].style.visibility = "visible";
+   stars[1].style.visibility = "visible";
+   stars[2].style.visibility = "visible";
+
+   for (let star of stars) {
+     starsMain.appendChild(star);
+   }
+
+   // initializes the vars
+   clicks = 0;
+   selectedCards = [];
+   matchedPairs = 0;
+   seconds = 0;
+
+   // initializes the modal components
+   while (starsModal.firstChild) {
+     starsModal.removeChild(starsModal.firstChild);
+   }
+
+   // shuffles the cards
+   var shCards = shuffle(cards);
+
+   // assembles the deck
+   for (let card of shCards) {
+     card.classList.remove("show", "open", "match");
+     deck.appendChild(card);
+   }
+}
+
+
+// This function verifies if the select cards are equal and sets the deck's reaction accordingly
+function isEqualCard (cardsArray) {
+
+  if (cardsArray[0].isEqualNode(cardsArray[1])) {
+      cardsArray[0].classList.toggle("match");
+      cardsArray[1].classList.toggle("match");
+      return true;
+  } else {
+      setTimeout(function() {
+      cardsArray[0].classList.remove("show", "open")
+      cardsArray[1].classList.remove("show", "open");
+      return false;
+    },500);
+  }
+}
+
+
+// This functions sets the Congratulations Modal
+function setModal() {
+
+  // stops the timer
+  clearInterval(timerId);
+
+  // shows the modal, total time and rating
+  modal.style.visibility = "visible";
+  modalTime.innerHTML=(parseInt(seconds/60,10) + ":" + seconds%60);
+
+  for (let star of stars) {
+    starsModal.appendChild(star);
+  }
+}
+
+
+// This functions does click related verifications
+function verifyClicks() {
+
+  // Verifies if it's the first captured click to start the timer
+  if (clicks === 1) {
+      timerId = setInterval( function(){
+                      ++seconds;
+                      timer.innerHTML=(parseInt(seconds/60,10) + ":" + seconds%60);
+                    }, 1000);
+  }
+
+  // Verfifes the quantities of clicks to rate the player using the stars
+  if (clicks <= 60) {
+      if (clicks > 16 && clicks <= 17) {
+          stars[2].style.visibility = "collapse";
+      } else if (clicks >= 18 && clicks <= 25) {
+          stars[1].style.visibility = "collapse";
+      } else if (clicks > 60) {
+          stars[0].style.visibility = "collapse";
+      }
+  }
+
+  // Verifies if the player has finished the game by matching all the pairs
+  if (matchedPairs === 8) {
+      setModal();
+  }
+
+}
+
+
+// Flips the received card and verifies the cards/clicks related conditions of the game
+function showCard(card) {
+  ++clicks;
+
+  // flips the selected card
+  card.classList.toggle("show");
+  card.classList.toggle("open");
+
+  // pushes it into an array
+  selectedCards.push(card);
+
+  // sets the moves counter
+  document.querySelector(".moves").textContent = clicks;
+
+  //verifies the amount of selected cars to verify if they are equal
+  if (selectedCards.length === 2) {
+      if (isEqualCard(selectedCards)) {
+        ++matchedPairs;
+      }
+      selectedCards = [];
+  }
+
+  verifyClicks();
+}
+
+
+// This section of the program sets the events listeners and once the browser has loaded, initializes the game.
+  restartButton.addEventListener("click", initializeGame);
+  playAgain.addEventListener("click", initializeGame);
+
+  for (let card of cards) {
+    card.addEventListener("click", function(){
+                                if (!card.classList.contains("show")) {
+                                    showCard(card);
+                                } });
+  }
+
+  window.onload = initializeGame;
+// end-of-section
